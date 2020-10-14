ece1907f31bc9ac9bb705be4fb21d74a1d320989
Ryan Oldenburg <guzba8@gmail.com>
2020-10-13 23:09:43 -0500

    benchmark in progress

---
+++ tests/benchmark.nim
@@ -0,0 +1,17 @@
+import strformat, zippy, fidget/opengl/perf
+
+
+proc t() =
+  # let file = "alice29.txt.z"
+  let file = "urls.10K.z"
+  # let file = "test.z"
+  let compressed = readFile(&"tests/data/{file}")
+  let uncompressed = uncompress(compressed)
+  echo uncompressed.len
+  # echo cast[string](uncompressed)
+  # let compressed = compress(original, level=11)
+  # writeFile(&"tests/data/{file}.z", compressed)
+
+timeIt "test":
+  for i in 0 ..< 25:
+    t()
