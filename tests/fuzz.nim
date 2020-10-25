9a1d02e32494a432734fb86ff579f74bbb0e948d
Ryan Oldenburg <guzba8@gmail.com>
2020-10-25 16:07:48 -0500

    fuzz

---
+++ tests/fuzz.nim
@@ -0,0 +1,28 @@
+import strformat, random, zippy
+
+randomize()
+
+const files = [
+  "randtest1.z",
+  "randtest2.z",
+  "randtest3.z",
+  "rfctest1.z",
+  "rfctest2.z",
+  "rfctest3.z",
+  "zerotest1.z",
+  "zerotest2.z",
+  "zerotest3.z",
+]
+
+for i in 0 ..< 10_000:
+  let file = files[rand(files.len - 1)]
+  var compressed = readFile(&"tests/data/{file}")
+  let
+    pos = rand(compressed.len - 1)
+    value = rand(255).char
+  compressed[pos] = value
+  echo &"{i} {file} {pos} {value.uint8}"
+  try:
+    assert uncompress(compressed).len > 0
+  except ZippyError:
+    discard
