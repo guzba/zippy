c751fc0aa0c4cab68c601ec1288af60053d741e4
Ryan Oldenburg <guzba8@gmail.com>
2020-11-06 16:59:00 -0600

    works, remove old lz77 impl

--- tests/lz77.nim
+++ tests/lz77.nim
@@ -3,7 +3,7 @@ import strformat, strutils, fidget/opengl/perf
 const
   minMatchLen = 3
 
-  windowSize = 1 shl 16
+  windowSize = 1 shl 15
   maxMatchLen = 258
   goodMatchLen = 32
   maxChainLen = 1024
@@ -17,8 +17,6 @@ var
   totalMatch: int
 
 proc lz77Encode2(src: seq[uint8]): seq[uint16] =
-  echo windowSize
-
   # assert windowSize <= maxWindowSize
   assert (windowSize and (windowSize - 1)) == 0
   assert (hashSize and hashMask) == 0
@@ -63,7 +61,7 @@ proc lz77Encode2(src: seq[uint8]): seq[uint16] =
     updateChain()
 
     var
-      hashPos = chain[pos]
+      hashPos = chain[windowPos]
       stop = min(src.len, pos + maxMatchLen)
       chainLen, prevOffset, longestMatchOffset, longestMatchLen: int
     while true:
@@ -71,27 +69,33 @@ proc lz77Encode2(src: seq[uint8]): seq[uint16] =
         break
       inc chainLen
 
-      let offset = pos - hashPos
-      if offset == 0:
-        break
+      let offset =
+        if hashPos <= windowPos:
+          windowPos - hashPos
+        else:
+          windowPos - hashPos + windowSize
 
-      var matchLen: int
-      for i in 0 ..< stop - pos:
-        if src[hashPos + i] != src[pos + i]:
+      if offset < prevOffset:
+        break
+      prevOffset = offset
+
+      if offset > 0:
+        var matchLen: int
+        for i in 0 ..< stop - pos:
+          if src[pos - offset + i] != src[pos + i]:
+            break
+          inc matchLen
+
+        if matchLen > longestMatchLen:
+          longestMatchLen = matchLen
+          longestMatchOffset = offset
+
+        if longestMatchLen >= goodMatchLen:
           break
-        inc matchLen
 
-      if matchLen > longestMatchLen:
-        longestMatchLen = matchLen
-        longestMatchOffset = offset
-
-      if longestMatchLen >= goodMatchLen:
+      if hashPos == chain[hashPos]:
         break
-
-      let nextPos = chain[hashPos]
-      if hashPos == nextPos:
-        break
-      hashPos = nextPos
+      hashPos = chain[hashPos]
 
     if longestMatchLen > minMatchLen:
       addLookBack(result, longestMatchOffset, longestMatchLen)
@@ -133,128 +137,30 @@ proc lz77Decode2(src: seq[uint8], encoded: seq[uint16]): seq[uint8] =
 
   result.setLen(op)
 
-proc lz77Encode(src: seq[uint8]): seq[uint16] =
-  result.setLen(src.len div 2)
-
-  var pos, windowStart, matchStart, matchOffset, matchLen: int
-  for i, c in src:
-    if pos + 3 >= result.len:
-      result.setLen(result.len * 2)
-
-    template emit() =
-      if matchLen >= minMatchLen:
-        # debugEcho &"<{matchOffset},{matchLen}>"
-        result[pos] = uint16.high
-        result[pos + 1] = matchOffset.uint16
-        result[pos + 2] = matchLen.uint16
-        inc(pos, 3)
-        inc(totalMatch, matchLen)
-      else:
-        for j in 0 ..< matchLen:
-          # debugEcho src[windowStart + matchStart + j].char
-          result[pos] = src[windowStart + matchStart + j]
-          inc pos
-      matchLen = 0
-
-    func find(
-      src: seq[uint8], value: uint8, start, stop: int
-    ): int {.inline.} =
-      result = -1
-      for j in start ..< stop:
-        if src[j] == value:
-          result = j - start
-          break
-
-    if matchLen > 0:
-      if src[windowStart + matchStart + matchLen] == c:
-        inc matchLen
-        if matchLen == maxMatchLen or i == src.high:
-          emit()
-          # We've consumed this c so don't hit the matchLen == 0 block
-          continue
-      else:
-        emit()
-
-    if matchLen == 0:
-      windowStart = max(i - windowSize, 0)
-      let index = src.find(c, windowStart, i)
-      if index >= 0:
-        matchStart = index
-        matchOffset = i - windowStart - index
-        inc matchLen
-        if i == src.high:
-          emit()
-      else:
-        # debugEcho c.char
-        result[pos] = c
-        inc pos
-
-  result.setLen(pos)
-
-proc lz77Decode(encoded: seq[uint16]): seq[uint8] =
-  result.setLen(encoded.len)
-
-  var ip, op: int
-  while ip < encoded.len:
-    if op >= result.len:
-      result.setLen(result.len * 2)
-
-    if encoded[ip] == uint16.high:
-      let
-        offset = encoded[ip + 1].int
-        length = encoded[ip + 2].int
-      # debugEcho &"<{offset},{length}>"
-      inc(ip, 3)
-
-      var copyPos = op - offset
-      if op + length > result.len:
-        result.setLen(max(result.len * 2, result.len + length))
-      for j in 0 ..< length:
-        result[op + j] = result[copyPos + j]
-      inc(op, length)
-    else:
-      # debugEcho encoded[ip].char
-      result[op] = encoded[ip].uint8
-      inc ip
-      inc op
-
-  result.setLen(op)
-
 const files = [
-  # "randtest1.gold",
-  # "randtest2.gold",
-  # "randtest3.gold",
+  "randtest1.gold",
+  "randtest2.gold",
+  "randtest3.gold",
   "rfctest1.gold",
-  # "rfctest2.gold",
-  # "rfctest3.gold",
-  # # "tor-list.gold",
-  # "zerotest1.gold",
-  # "zerotest2.gold",
-  # "zerotest3.gold",
-  # "empty.gold",
-  # "alice29.txt",
-  # "asyoulik.txt",
-  # "fireworks.jpg",
-  # "geo.protodata",
-  # "html",
-  # "html_x_4",
-  # "kppkn.gtb",
-  # "lcet10.txt",
-  # "paper-100k.pdf",
-  # "plrabn12.txt",
-  # "urls.10K"
+  "rfctest2.gold",
+  "rfctest3.gold",
+  "tor-list.gold",
+  "zerotest1.gold",
+  "zerotest2.gold",
+  "zerotest3.gold",
+  "empty.gold",
+  "alice29.txt",
+  "asyoulik.txt",
+  "fireworks.jpg",
+  "geo.protodata",
+  "html",
+  "html_x_4",
+  "kppkn.gtb",
+  "lcet10.txt",
+  "paper-100k.pdf",
+  "plrabn12.txt",
+  "urls.10K"
 ]
-
-# timeIt "lz77":
-#   for i in 0 ..< 1:
-#     for file in files:
-#       let
-#         original = cast[seq[uint8]](readFile(&"tests/data/{file}"))
-#         encoded = lz77Encode(original)
-#         decoded = lz77Decode(encoded)
-#       echo &"{file} original: {original.len} encoded: {encoded.len}"
-#       echo totalMatch
-#       doAssert original == decoded
 
 timeIt "lz77_2":
   for i in 0 ..< 1:
@@ -264,19 +170,20 @@ timeIt "lz77_2":
         encoded = lz77Encode2(original)
         decoded = lz77Decode2(original, encoded)
       echo &"{file} original: {original.len} encoded: {encoded.len}"
-      echo "totalMatch: ", totalMatch
-      echo "decoded: ", decoded.len
-      if original != decoded:
-        for i in 0 ..< original.len:
-          if original[i] != decoded[i]:
-            echo "bad @ ", i, " ", original[i], " ", decoded[i]
-            break
+      # echo "totalMatch: ", totalMatch
+      # echo "decoded: ", decoded.len
+      assert original == decoded
+      # if original != decoded:
+      #   for i in 0 ..< original.len:
+      #     if original[i] != decoded[i]:
+      #       echo "bad @ ", i, " ", original[i], " ", decoded[i]
+      #       break
 
 # let
-#   original = cast[seq[uint8]]("zSAM SAM SAMz")
+#   original = cast[seq[uint8]]("zSAM SAM SAM a SAM SAM SAMz")
 #   encoded = lz77Encode2(original)
 # echo &"original: {original.len} encoded: {encoded.len}"
 # echo totalMatch
 # echo encoded
-# echo lz77Decode2(original, encoded)
+# echo cast[string](lz77Decode2(original, encoded))
 # doAssert original == lz77Decode2(original, encoded)
