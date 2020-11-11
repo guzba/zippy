8976eadf9860ccba5cf151962e4a356ecb9cccb9
Ryan Oldenburg <guzba8@gmail.com>
2020-11-11 17:09:07 -0600

    test levels

---
+++ tests/test_levels.nim
@@ -0,0 +1,25 @@
+import strformat, zippy
+
+const
+  golds = [
+    "randtest1.gold",
+    "rfctest1.gold",
+    "zerotest1.gold",
+    "empty.gold",
+    "alice29.txt",
+    "asyoulik.txt",
+    "fireworks.jpg",
+    "geo.protodata",
+    "html",
+    "kppkn.gtb",
+    "paper-100k.pdf"
+  ]
+
+for level in -2 .. 9:
+  for gold in golds:
+    let
+      original = readFile(&"tests/data/{gold}")
+      compressed = compress(original, level)
+      uncompressed = uncompress(compressed)
+    echo &"Level {level} {gold} original: {original.len} compressed: {compressed.len}"
+    doAssert original == uncompressed
