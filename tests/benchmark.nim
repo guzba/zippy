532ee37c40e8c7092e275ff8c6d73043d93256e6
Ryan Oldenburg <guzba8@gmail.com>
2020-12-04 01:12:03 -0600

    better benchmarking

--- tests/benchmark.nim
+++ tests/benchmark.nim
@@ -1,6 +1,6 @@
-import std/monotimes, strformat, zip/zlib, zippy
+import benchy, strformat, zip/zlib, zippy
 
-# import miniz, nimPNG/nimz
+import miniz, nimPNG/nimz
 
 const
   zs = [
@@ -20,178 +20,88 @@ const
     "geo.protodata",
     "gzipfiletest.txt"
   ]
-  iterations = 1000
+  iterations = 10
 
-block guzba_zippy_compress:
-  echo "https://github.com/guzba/zippy compress [default]"
-  for gold in golds:
-    let
-      uncompressed = readFile(&"tests/data/{gold}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/guzba/zippy compress [default]"
+for gold in golds:
+  timeIt gold:
+    let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      let compressed = zippy.compress(uncompressed, dataFormat = dfZlib)
-      inc(c, compressed.len)
-    let
-      delta = float64(getMonoTime().ticks - start) / 1000000000.0
-      reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-    echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+      keep zippy.compress(uncompressed, dataFormat = dfZlib)
 
-block nimlang_zip_compress: # Requires zlib1.dll
-  echo "https://github.com/nim-lang/zip compress [default]"
-  for gold in golds:
-    let
-      uncompressed = readFile(&"tests/data/{gold}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/nim-lang/zip compress [default]" # Requires zlib1.dll
+for gold in golds:
+  timeIt gold:
+    let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      let compressed = zlib.compress(uncompressed, stream = ZLIB_STREAM)
-      inc(c, compressed.len)
-    let
-      delta = float64(getMonoTime().ticks - start) / 1000000000.0
-      reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-    echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+      keep zlib.compress(uncompressed, stream = ZLIB_STREAM)
 
-# block treeform_miniz_compress:
-#   echo "https://github.com/treeform/miniz compress"
-#   for gold in golds:
-#     let
-#       uncompressed = readFile(&"tests/data/{gold}")
-#       start = getMonoTime().ticks
-#     var c: int
+# echo "https://github.com/treeform/miniz compress [default]"
+# for gold in golds:
+#   timeIt gold:
+#     let uncompressed = readFile(&"tests/data/{gold}")
 #     for i in 0 ..< iterations:
-#       let compressed = miniz.compress(uncompressed, 1)
-#       inc(c, compressed.len)
-#     let
-#       delta = float64(getMonoTime().ticks - start) / 1000000000.0
-#       reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-#     echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+#       keep miniz.compress(uncompressed)
 
-# block jangko_nimPNG_compress:
-#   echo "https://github.com/jangko/nimPNG compress"
-#   for gold in golds:
-#     let
-#       uncompressed = readFile(&"tests/data/{gold}")
-#       start = getMonoTime().ticks
-#     var c: int
+# echo "https://github.com/jangko/nimPNG compress [default]"
+# for gold in golds:
+#   timeIt gold:
+#     let uncompressed = readFile(&"tests/data/{gold}")
 #     for i in 0 ..< iterations:
-#       let compressed = zlib_compress(nzDeflateInit(uncompressed))
-#       inc(c, compressed.len)
-#     let
-#       delta = float64(getMonoTime().ticks - start) / 1000000000.0
-#       reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-#     echo &"  {gold}: {delta:.4f}s {reduction:0.2f}%"
+#       keep zlib_compress(nzDeflateInit(uncompressed))
 
-block guzba_zippy_compress:
-  echo "https://github.com/guzba/zippy compress [best speed]"
-  for gold in golds:
-    let
-      uncompressed = readFile(&"tests/data/{gold}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/guzba/zippy compress [best speed]"
+for gold in golds:
+  timeIt gold:
+    let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      let compressed = zippy.compress(uncompressed, BestSpeed, dfZlib)
-      inc(c, compressed.len)
-    let
-      delta = float64(getMonoTime().ticks - start) / 1000000000.0
-      reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-    echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+      keep zippy.compress(uncompressed, BestSpeed, dataFormat = dfZlib)
 
-block nimlang_zip_compress: # Requires zlib1.dll
-  echo "https://github.com/nim-lang/zip compress [best speed]"
-  for gold in golds:
-    let
-      uncompressed = readFile(&"tests/data/{gold}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/nim-lang/zip compress [best speed]" # Requires zlib1.dll
+for gold in golds:
+  timeIt gold:
+    let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      let compressed = zlib.compress(uncompressed, Z_BEST_SPEED, ZLIB_STREAM)
-      inc(c, compressed.len)
-    let
-      delta = float64(getMonoTime().ticks - start) / 1000000000.0
-      reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-    echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+      keep zlib.compress(uncompressed, Z_BEST_SPEED, ZLIB_STREAM)
 
-block guzba_zippy_compress:
-  echo "https://github.com/guzba/zippy compress [best compression]"
-  for gold in golds:
-    let
-      uncompressed = readFile(&"tests/data/{gold}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/guzba/zippy compress [best compression]"
+for gold in golds:
+  timeIt gold:
+    let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      let compressed = zippy.compress(uncompressed, BestCompression, dfZlib)
-      inc(c, compressed.len)
-    let
-      delta = float64(getMonoTime().ticks - start) / 1000000000.0
-      reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-    echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+      keep zippy.compress(uncompressed, BestCompression, dataFormat = dfZlib)
 
-block nimlang_zip_compress: # Requires zlib1.dll
-  echo "https://github.com/nim-lang/zip compress [best compression]"
-  for gold in golds:
-    let
-      uncompressed = readFile(&"tests/data/{gold}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/nim-lang/zip compress [best compression]" # Requires zlib1.dll
+for gold in golds:
+  timeIt gold:
+    let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      let compressed = zlib.compress(
-        uncompressed, Z_BEST_COMPRESSION, ZLIB_STREAM
-      )
-      inc(c, compressed.len)
-    let
-      delta = float64(getMonoTime().ticks - start) / 1000000000.0
-      reduction = 100 - (c / (uncompressed.len * iterations)) * 100
-    echo &"  {gold}: {delta:.4f}s {(reduction):0.2f}%"
+      keep zlib.compress(uncompressed, Z_BEST_COMPRESSION, ZLIB_STREAM)
 
-block guzba_zippy_uncompress:
-  echo "https://github.com/guzba/zippy uncompress"
-  for z in zs:
-    let
-      compressed = readFile(&"tests/data/{z}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/guzba/zippy uncompress"
+for z in zs:
+  timeIt z:
+    let compressed = readFile(&"tests/data/{z}")
     for i in 0 ..< iterations:
-      let uncompressed = zippy.uncompress(compressed)
-      inc(c, uncompressed.len)
-    let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-    echo &"  {z}: {delta:.4f}s [{c}]"
+      keep zippy.uncompress(compressed)
 
-block nimlang_zip_uncompress: # Requires zlib1.dll
-  echo "https://github.com/nim-lang/zip uncompress"
-  for z in zs:
-    let
-      compressed = readFile(&"tests/data/{z}")
-      start = getMonoTime().ticks
-    var c: int
+echo "https://github.com/nim-lang/zip uncompress" # Requires zlib1.dll
+for z in zs:
+  timeIt z:
+    let compressed = readFile(&"tests/data/{z}")
     for i in 0 ..< iterations:
-      let uncompressed = zlib.uncompress(compressed, stream = ZLIB_STREAM)
-      inc(c, uncompressed.len)
-    let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-    echo &"  {z}: {delta:.4f}s [{c}]"
+      keep zlib.uncompress(compressed, stream = ZLIB_STREAM)
 
-# block treeform_miniz_uncompress:
-#   echo "https://github.com/treeform/miniz uncompress"
-#   for z in zs:
-#     let
-#       compressed = readFile(&"tests/data/{z}")
-#       start = getMonoTime().ticks
-#     var c: int
+# echo "https://github.com/treeform/miniz uncompress"
+# for z in zs:
+#   timeIt z:
+#     let compressed = readFile(&"tests/data/{z}")
 #     for i in 0 ..< iterations:
-#       let uncompressed = miniz.uncompress(compressed)
-#       inc(c, uncompressed.len)
-#     let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-#     echo &"  {z}: {delta:.4f}s [{c}]"
+#       keep miniz.uncompress(compressed)
 
-# block jangko_nimPNG_uncompress:
-#   echo "https://github.com/jangko/nimPNG uncompress"
-#   for z in zs:
-#     let
-#       compressed = readFile(&"tests/data/{z}")
-#       start = getMonoTime().ticks
-#     var c: int
+# echo "https://github.com/jangko/nimPNG uncompress"
+# for z in zs:
+#   timeIt z:
+#     let compressed = readFile(&"tests/data/{z}")
 #     for i in 0 ..< iterations:
-#       let uncompressed = zlib_decompress(nzInflateInit(compressed))
-#       inc(c, uncompressed.len)
-#     let delta = float64(getMonoTime().ticks - start) / 1000000000.0
-#     echo &"  {z}: {delta:.4f}s [{c}]"
+#       keep zlib_decompress(nzInflateInit(compressed))
