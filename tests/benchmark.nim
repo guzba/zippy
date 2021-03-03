c922587070bd2f4eccc9183bebd966082428d5b9
Ryan Oldenburg <guzba8@gmail.com>
2021-03-02 22:36:45 -0600

    benchmark without keep

--- tests/benchmark.nim
+++ tests/benchmark.nim
@@ -27,81 +27,81 @@ for gold in golds:
   timeIt gold:
     let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      keep zippy.compress(uncompressed, dataFormat = dfZlib)
+      discard zippy.compress(uncompressed, dataFormat = dfZlib)
 
 echo "https://github.com/nim-lang/zip compress [default]" # Requires zlib1.dll
 for gold in golds:
   timeIt gold:
     let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      keep zlib.compress(uncompressed, stream = ZLIB_STREAM)
+      discard zlib.compress(uncompressed, stream = ZLIB_STREAM)
 
 # echo "https://github.com/treeform/miniz compress [default]"
 # for gold in golds:
 #   timeIt gold:
 #     let uncompressed = readFile(&"tests/data/{gold}")
 #     for i in 0 ..< iterations:
-#       keep miniz.compress(uncompressed)
+#       discard miniz.compress(uncompressed)
 
 # echo "https://github.com/jangko/nimPNG compress [default]"
 # for gold in golds:
 #   timeIt gold:
 #     let uncompressed = readFile(&"tests/data/{gold}")
 #     for i in 0 ..< iterations:
-#       keep zlib_compress(nzDeflateInit(uncompressed))
+#       discard zlib_compress(nzDeflateInit(uncompressed))
 
 echo "https://github.com/guzba/zippy compress [best speed]"
 for gold in golds:
   timeIt gold:
     let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      keep zippy.compress(uncompressed, BestSpeed, dataFormat = dfZlib)
+      discard zippy.compress(uncompressed, BestSpeed, dataFormat = dfZlib)
 
 echo "https://github.com/nim-lang/zip compress [best speed]" # Requires zlib1.dll
 for gold in golds:
   timeIt gold:
     let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      keep zlib.compress(uncompressed, Z_BEST_SPEED, ZLIB_STREAM)
+      discard zlib.compress(uncompressed, Z_BEST_SPEED, ZLIB_STREAM)
 
 echo "https://github.com/guzba/zippy compress [best compression]"
 for gold in golds:
   timeIt gold:
     let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      keep zippy.compress(uncompressed, BestCompression, dataFormat = dfZlib)
+      discard zippy.compress(uncompressed, BestCompression, dataFormat = dfZlib)
 
 echo "https://github.com/nim-lang/zip compress [best compression]" # Requires zlib1.dll
 for gold in golds:
   timeIt gold:
     let uncompressed = readFile(&"tests/data/{gold}")
     for i in 0 ..< iterations:
-      keep zlib.compress(uncompressed, Z_BEST_COMPRESSION, ZLIB_STREAM)
+      discard zlib.compress(uncompressed, Z_BEST_COMPRESSION, ZLIB_STREAM)
 
 echo "https://github.com/guzba/zippy uncompress"
 for z in zs:
   timeIt z:
     let compressed = readFile(&"tests/data/{z}")
     for i in 0 ..< iterations:
-      keep zippy.uncompress(compressed)
+      discard zippy.uncompress(compressed)
 
 echo "https://github.com/nim-lang/zip uncompress" # Requires zlib1.dll
 for z in zs:
   timeIt z:
     let compressed = readFile(&"tests/data/{z}")
     for i in 0 ..< iterations:
-      keep zlib.uncompress(compressed, stream = ZLIB_STREAM)
+      discard zlib.uncompress(compressed, stream = ZLIB_STREAM)
 
 # echo "https://github.com/treeform/miniz uncompress"
 # for z in zs:
 #   timeIt z:
 #     let compressed = readFile(&"tests/data/{z}")
 #     for i in 0 ..< iterations:
-#       keep miniz.uncompress(compressed)
+#       discard miniz.uncompress(compressed)
 
 # echo "https://github.com/jangko/nimPNG uncompress"
 # for z in zs:
 #   timeIt z:
 #     let compressed = readFile(&"tests/data/{z}")
 #     for i in 0 ..< iterations:
-#       keep zlib_decompress(nzInflateInit(compressed))
+#       discard zlib_decompress(nzInflateInit(compressed))
