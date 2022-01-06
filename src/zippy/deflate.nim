ef9b8bb0ba900d67f4341c644f273bbc4d8e3deb
Ryan Oldenburg <ryan@guzba.com>
2022-01-06 00:35:45 -0600

    f

--- src/zippy/deflate.nim
+++ src/zippy/deflate.nim
@@ -10,7 +10,6 @@ func huffmanCodeLengths(
   ## https://en.wikipedia.org/wiki/Package-merge_algorithm#Reduction_of_length-limited_Huffman_coding_to_the_coin_collector%27s_problem
   ## https://en.wikipedia.org/wiki/Canonical_Huffman_code
 
-  # When to use the fixed Huffman codes? If ever.
   # This is the slow part of deflating small files.
 
   type Coin = object
