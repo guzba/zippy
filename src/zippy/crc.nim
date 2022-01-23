34943dcdc2af36ba926bd7a6d5614cca49680d4b
Ryan Oldenburg <ryan@guzba.com>
2022-01-23 02:21:54 -0600

    f

--- src/zippy/crc.nim
+++ src/zippy/crc.nim
@@ -2,25 +2,24 @@ import internal
 
 ## See https://create.stephan-brumme.com/crc32/
 
-const
-  crcTables = block:
-    var
-      tables: array[8, array[256, uint32]]
-      c: uint32
-    for i in 0.uint32 ..< 256:
-      c = i
-      for j in 0 ..< 8:
-        c = (c shr 1) xor ((c and 1) * 0xedb88320.uint32)
-      tables[0][i] = c
-    for i in 0 ..< 256:
-      tables[1][i] = (tables[0][i] shr 8) xor tables[0][tables[0][i] and 255]
-      tables[2][i] = (tables[1][i] shr 8) xor tables[0][tables[1][i] and 255]
-      tables[3][i] = (tables[2][i] shr 8) xor tables[0][tables[2][i] and 255]
-      tables[4][i] = (tables[3][i] shr 8) xor tables[0][tables[3][i] and 255]
-      tables[5][i] = (tables[4][i] shr 8) xor tables[0][tables[4][i] and 255]
-      tables[6][i] = (tables[5][i] shr 8) xor tables[0][tables[5][i] and 255]
-      tables[7][i] = (tables[6][i] shr 8) xor tables[0][tables[6][i] and 255]
-    tables
+const crcTables = block:
+  var
+    tables: array[8, array[256, uint32]]
+    c: uint32
+  for i in 0.uint32 ..< 256:
+    c = i
+    for j in 0 ..< 8:
+      c = (c shr 1) xor ((c and 1) * 0xedb88320.uint32)
+    tables[0][i] = c
+  for i in 0 ..< 256:
+    tables[1][i] = (tables[0][i] shr 8) xor tables[0][tables[0][i] and 255]
+    tables[2][i] = (tables[1][i] shr 8) xor tables[0][tables[1][i] and 255]
+    tables[3][i] = (tables[2][i] shr 8) xor tables[0][tables[2][i] and 255]
+    tables[4][i] = (tables[3][i] shr 8) xor tables[0][tables[3][i] and 255]
+    tables[5][i] = (tables[4][i] shr 8) xor tables[0][tables[4][i] and 255]
+    tables[6][i] = (tables[5][i] shr 8) xor tables[0][tables[5][i] and 255]
+    tables[7][i] = (tables[6][i] shr 8) xor tables[0][tables[6][i] and 255]
+  tables
 
 func crc32*(v: uint32, data: seq[uint8] | string): uint32 =
   result = v
