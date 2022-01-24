44fbe7758b62c2e8354fc0d46ebcaf1225dae21f
Ryan Oldenburg <ryan@guzba.com>
2022-01-24 16:09:32 -0600

    raw deflate

--- tests/benchmark.nim
+++ tests/benchmark.nim
@@ -25,37 +25,37 @@ echo "https://github.com/guzba/zippy compress [default]"
 for gold in golds:
   let uncompressed = readFile(&"tests/data/{gold}")
   timeIt gold:
-    discard zippy.compress(uncompressed, dataFormat = dfZlib)
+    discard zippy.compress(uncompressed, dataFormat = dfDeflate)
 
 echo "https://github.com/nim-lang/zip compress [default]" # Requires zlib1.dll
 for gold in golds:
   let uncompressed = readFile(&"tests/data/{gold}")
   timeIt gold:
-    discard zlib.compress(uncompressed, stream = ZLIB_STREAM)
+    discard zlib.compress(uncompressed, stream = RAW_DEFLATE)
 
 echo "https://github.com/guzba/zippy compress [best speed]"
 for gold in golds:
   let uncompressed = readFile(&"tests/data/{gold}")
   timeIt gold:
-    discard zippy.compress(uncompressed, BestSpeed, dataFormat = dfZlib)
+    discard zippy.compress(uncompressed, BestSpeed, dataFormat = dfDeflate)
 
 echo "https://github.com/nim-lang/zip compress [best speed]" # Requires zlib1.dll
 for gold in golds:
   let uncompressed = readFile(&"tests/data/{gold}")
   timeIt gold:
-    discard zlib.compress(uncompressed, Z_BEST_SPEED, ZLIB_STREAM)
+    discard zlib.compress(uncompressed, Z_BEST_SPEED, RAW_DEFLATE)
 
 echo "https://github.com/guzba/zippy compress [best compression]"
 for gold in golds:
   let uncompressed = readFile(&"tests/data/{gold}")
   timeIt gold:
-    discard zippy.compress(uncompressed, BestCompression, dataFormat = dfZlib)
+    discard zippy.compress(uncompressed, BestCompression, dataFormat = dfDeflate)
 
 echo "https://github.com/nim-lang/zip compress [best compression]" # Requires zlib1.dll
 for gold in golds:
   let uncompressed = readFile(&"tests/data/{gold}")
   timeIt gold:
-    discard zlib.compress(uncompressed, Z_BEST_COMPRESSION, ZLIB_STREAM)
+    discard zlib.compress(uncompressed, Z_BEST_COMPRESSION, RAW_DEFLATE)
 
 echo "https://github.com/guzba/zippy uncompress"
 for z in zs:
@@ -67,4 +67,4 @@ echo "https://github.com/nim-lang/zip uncompress" # Requires zlib1.dll
 for z in zs:
   let compressed = readFile(&"tests/data/{z}")
   timeIt z:
-    discard zlib.uncompress(compressed, stream = ZLIB_STREAM)
+    discard zlib.uncompress(compressed)
