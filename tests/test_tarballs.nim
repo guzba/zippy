7a44fcdbcb306844ba6dbc107a866cbc170e3899
Ryan Oldenburg <ryan@guzba.com>
2022-01-29 01:43:22 -0600

    test work

--- tests/test_tarballs.nim
+++ tests/test_tarballs.nim
@@ -5,23 +5,26 @@ removeDir("tmp/tar")
 createDir("tmp/tar")
 extractAll("tests/data/tarballs/Nim-1.6.2.tar.gz", "tmp/tar/zippy")
 
-when not defined(windows): # tar exists but does not work on Windows
-  createDir("tmp/tar/gold")
-  let cmd = "tar -xf tests/data/ziparchives/nim-1.6.2_x64.zip -C tmp/tar/gold"
-  doAssert execShellCmd(cmd) == 0
+createDir("tmp/tar/gold")
+let cmd = "tar -xf tests/data/tarballs/Nim-1.6.2.tar.gz -C tmp/tar/gold"
+doAssert execShellCmd(cmd) == 0
 
-  for path in walkDirRec("tmp/tar/gold", relative = true):
-    let
-      goldPath = "tmp/tar/gold" / path
-      zippyPath = "tmp/tar/zippy" / path
+for path in walkDirRec("tmp/tar/gold", relative = true):
+  let
+    goldPath = "tmp/tar/gold" / path
+    zippyPath = "tmp/tar/zippy" / path
 
-    if dirExists(goldPath):
-      doAssert dirExists(zippyPath)
-    else:
-      doAssert fileExists(zippyPath)
-      doAssert readFile(goldPath) == readFile(zippyPath)
+  if dirExists(goldPath):
+    doAssert dirExists(zippyPath)
+  else:
+    when defined(windows):
+      # tar on Windows creates this monster, zippy handles this file correctly
+      if path == "Nim-1.6.2\\tests\\misc\\\226\148\156\195\145\226\148\156\195\177\226\148\156\226\149\162.nim":
+        continue
+    doAssert fileExists(zippyPath)
+    doAssert readFile(goldPath) == readFile(zippyPath)
 
-    doAssert getFilePermissions(goldPath) == getFilePermissions(zippyPath)
-    doAssert getLastModificationTime(goldPath) == getLastModificationTime(zippyPath)
+  doAssert getFilePermissions(goldPath) == getFilePermissions(zippyPath)
+  doAssert getLastModificationTime(goldPath) == getLastModificationTime(zippyPath)
 
 removeDir("tmp/tar")
