28790198b04c00d576195bf126fd7fe9d2b94e5b
Ryan Oldenburg <ryan@guzba.com>
2022-01-29 02:21:07 -0600

    update example

--- examples/tarball_extract.nim
+++ examples/tarball_extract.nim
@@ -1,6 +1,4 @@
-import os, zippy/tarballs
+import zippy/tarballs
 
 # Extracts all of the files and directories from the tarball.
-
-createDir("tmp") # Ensure the path to the output dir exists
-extractAll("tests/data/tarballs/Nim-1.4.2.tar.gz", "tmp/untarred")
+extractAll("tests/data/tarballs/Nim-1.6.2.tar.gz", "untarred")
