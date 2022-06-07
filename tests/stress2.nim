15f6d4d83244c02fa92f625133f6c0e044cd8ec4
Ryan Oldenburg <ryan@guzba.com>
2022-06-06 21:56:46 -0500

    add stress2 test

---
+++ tests/stress2.nim
@@ -0,0 +1,20 @@
+import std/random, std/times, zippy, zip/zlib
+
+let gold = readFile("tests/data/rfctest3.gold")
+
+let seed = epochTime().int
+var r = initRand(seed)
+
+for i in 0 ..< 10000:
+  echo "Test ", i, " (seed ", seed, ")"
+  let m = r.rand(100)
+
+  var data = gold
+  for i in 0 ..< m:
+    data &= gold
+
+  for level in [1, -1]: # BestSpeed and Default
+    var
+      compressed = zlib.compress(data, level = level, stream = GZIP_STREAM)
+      uncompressed = zippy.uncompress(compressed)
+    doAssert uncompressed == data
