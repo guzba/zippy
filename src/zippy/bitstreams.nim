15cb7c970c9cb520247792ec4060e582915381aa
Ryan Oldenburg <ryan@guzba.com>
2022-06-12 23:52:03 -0500

    fix nim 1.0.x

--- src/zippy/bitstreams.nim
+++ src/zippy/bitstreams.nim
@@ -18,7 +18,7 @@ when defined(release):
 
 proc fillBitBuffer*(b: var BitStreamReader) {.inline.} =
   let
-    bytesNeeded = cast[int]((64 - cast[uint](b.bitsBuffered)) div 8)
+    bytesNeeded = cast[int]((64.uint - cast[uint](b.bitsBuffered)) div 8)
     bytesAvailable = b.len - b.pos
 
   var src: uint64
