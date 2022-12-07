ae7ce384fc2ad167d766ad45ba1d7194fb74c531
Ryan Oldenburg <ryan@guzba.com>
2022-12-07 05:45:52 -0600

    updated

--- examples/ziparchive_explore.nim
+++ examples/ziparchive_explore.nim
@@ -2,7 +2,7 @@ import zippy/ziparchives
 
 # Open the zip archive. This only reads the records metadata,
 # it does not read the entire archive into memory.
-let reader = openZipArchive("tests/data/ziparchives/nim-1.6.2_x64.zip")
+let reader = openZipArchive("tests/data/ziparchives/Nim-1.6.6.zip")
 
 try:
   # Iterate over the paths in the zip archive.
@@ -10,7 +10,7 @@ try:
     echo path
 
   # Extract a file from the archive.
-  let contents = reader.extractFile("nim-1.6.2/doc/html/os.html")
+  let contents = reader.extractFile("Nim-1.6.6/doc/readme.txt")
   echo contents
 finally:
   # Remember to close the reader when done.
