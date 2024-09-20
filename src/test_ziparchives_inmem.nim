2031205beef8dde04e2ea1bd4e91dde9157efa77
Chris Collazo <ccollazo@geosonics.com>
2024-09-20 14:09:53 -0400

    Added ziparchives_inmem, which can extract from archives which have been read in as byte strings. This allows extracting from recursive archives in-memory.

---
+++ src/test_ziparchives_inmem.nim
@@ -0,0 +1,16 @@
+import zippy/ziparchives, zippy/ziparchives_inmem
+
+proc test_case() =
+  var archive = open_zip_archive("inner_test.zip")
+  defer: archive.close()
+    
+  for fname in archive.walk_files:
+    let bytes = archive.extract_file(fname)
+    var inner_archive = open_zip_archive_bytes(bytes)
+    defer: inner_archive.close()
+    
+    for ifname in inner_archive.walk_files:
+      let ifbytes = inner_archive.extract_file(ifname)
+      writeFile(ifname, ifbytes)
+
+test_case()
